944b78c9768b5eb78046a582bd48a56f46b85b31
春夏秋冬在中南 <2541093882@qq.com>
2023-04-25 08:44:35 +0800

    init子模块

---
+++ ui/conflict/src/main/java/com/laioffer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.laioffer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.laioffer.ui.conflict.R
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
