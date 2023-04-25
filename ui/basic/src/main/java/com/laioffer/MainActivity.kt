be972cfc2d9cfeadf1f22c1c97a6821456564a89
春夏秋冬在中南 <yuanmengdi@kuaishou.com>
2023-04-25 10:55:19 +0800

    init_ui模块

--- ui/basic/src/main/java/com/laioffer/MainActivity.kt
+++ ui/basic/src/main/java/com/laioffer/MainActivity.kt
@@ -2,7 +2,6 @@ package com.laioffer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.laioffer.ui.conflict.R
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
