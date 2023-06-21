96fc5ce7ecd23bd7f81b1f2e9d03dcdac62ec281
春夏秋冬在中南 <2541093882@qq.com>
2023-06-21 09:55:25 +0800

    异步任务和简单协程demo

---
+++ kotlin/coroutine/src/main/java/com/laioffer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.laioffer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.laioffer.coroutine.R
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
