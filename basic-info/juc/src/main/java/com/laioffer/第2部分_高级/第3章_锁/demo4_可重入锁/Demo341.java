38397d67e4590aa1adce689f51f43a0f525e10f2
春夏秋冬在中南 <2541093882@qq.com>
2023-08-07 08:45:45 +0800

    可重入锁

--- basic-info/juc/src/main/java/com/laioffer/第2部分_高级/第3章_锁/demo4_可重入锁/Demo341.java
+++ basic-info/juc/src/main/java/com/laioffer/第2部分_高级/第3章_锁/demo4_可重入锁/Demo341.java
@@ -4,7 +4,7 @@ package com.laioffer.第2部分_高级.第3章_锁.demo4_可重入锁;
  * 类的描述: 可重入锁: 同步代码块
  * Created by 春夏秋冬在中南 on 2023/7/17 22:56
  */
-public class Demo41 {
+public class Demo341 {
 
   public static void main(String[] args) {
     final Object object = new Object();
@@ -22,3 +22,12 @@ public class Demo41 {
     }, "线程1").start();
   }
 }
+
+/*
+线程1	 外层调用
+线程1	 中层调用
+线程1	 内层调用
+
+小结:
+    在一个synchronized修饰的方法或代码块内部调用本类的其它synchronized修饰的方法或者代码块时，是永远可以得到锁的
+ */
--- basic-info/juc/src/main/java/com/laioffer/第2部分_高级/第3章_锁/demo4_可重入锁/Demo343.java
+++ basic-info/juc/src/main/java/com/laioffer/第2部分_高级/第3章_锁/demo4_可重入锁/Demo343.java
@@ -6,7 +6,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * 类的描述: 可重入锁: ReentrantLock
  * Created by 春夏秋冬在中南 on 2023/7/18 23:19
  */
-public class Demo43 {
+public class Demo343 {
 
   private static ReentrantLock lock = new ReentrantLock();
 
