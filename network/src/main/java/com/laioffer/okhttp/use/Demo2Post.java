39684071c510548dbc086ff1f1ec83593ec21f7a
春夏秋冬在中南 <2541093882@qq.com>
2023-08-10 23:28:08 +0800

    更新okhttp使用的代码

--- network/src/main/java/com/laioffer/okhttp/use/Demo2Post.java
+++ network/src/main/java/com/laioffer/okhttp/use/Demo2Post.java
@@ -1,4 +1,4 @@
-package com.laioffer.okhttp;
+package com.laioffer.okhttp.use;
 
 import android.os.Bundle;
 
@@ -16,45 +16,37 @@ import okhttp3.Response;
 import okhttp3.ResponseBody;
 
 /**
- * 类的描述: okhttp使用
- * Created by 春夏秋冬在中南 on 2023/6/18 16:06
+ * 类的描述: okhttp使用2: POST请求
+ * Created by 春夏秋冬在中南 on 2023/8/10 01:57
  */
-public class Demo1Activity extends AppCompatActivity {
+public class Demo2Post extends AppCompatActivity {
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
   }
 
-  /**
-   * OkHttp GET请求
-   */
-  private void requestGET() throws IOException {
-    OkHttpClient client = new OkHttpClient();
-    Request request = new Request.Builder()
-        .url("xxx")
-        .build();
-    Call call = client.newCall(request);
-    // 执行同步请求
-    // 调用execute()后，代码会阻塞在这里，直到服务器有了响应之后，才能继续往下走，得到response
-    Response response = call.execute();
-
-    ResponseBody body = response.body();
-    System.out.println("接口Response=" + body);
-  }
 
   /**
    * OkHttp POST请求
    */
   private void requestPOST() throws IOException {
+    // 1.创建OKHttpClient对象
     OkHttpClient client = new OkHttpClient();
+
+    // 2.构建FormBody，传入参数
     RequestBody requestBody = new FormBody.Builder()
         .add("name", "张三")
         .build();
+
+    // 3.构建Request，将FormBody作为POST方法的参数传入
     Request request = new Request.Builder()
         .url("xxx")
         .post(requestBody)
         .build();
+
+    // 4.将Request封装为Call
     Call call = client.newCall(request);
     Response response = call.execute();
     ResponseBody body = response.body();
