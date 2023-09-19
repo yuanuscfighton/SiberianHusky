0dc59b1159922d4a4b0b8f2baaf9c5268dbe4354
春夏秋冬在中南 <2541093882@qq.com>
2023-09-20 07:59:11 +0800

    List集合

--- basic-info/juc/src/main/java/com/laioffer/第1部分_基础/第4章_集合线程安全/Demo3.java
+++ basic-info/juc/src/main/java/com/laioffer/第1部分_基础/第4章_集合线程安全/Demo3.java
@@ -1,4 +1,4 @@
-package com.laioffer.demo2;
+package com.laioffer.第1部分_基础.第4章_集合线程安全;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -6,10 +6,10 @@ import java.util.List;
 import java.util.UUID;
 
 /**
- * 类的描述: 集合线程不安全演示
+ * 类的描述: Collections#synchronizedList - 线程安全
  * Created by 春夏秋冬在中南 on 2023/6/4 08:37
  */
-public class Client {
+public class Demo3 {
   public static void main(String[] args) {
     // List<String> list = new ArrayList<>();
     List<String> list = Collections.synchronizedList(new ArrayList<>());
