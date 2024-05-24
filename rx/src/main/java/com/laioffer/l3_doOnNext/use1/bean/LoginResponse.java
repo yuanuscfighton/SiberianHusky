4953b72e5d292268bf6bfe9e7449994ea689641e
春夏秋冬在中南 <2541093882@qq.com>
2024-05-25 02:00:40 +0800

    20240524

---
+++ rx/src/main/java/com/laioffer/l3_doOnNext/use1/bean/LoginResponse.java
@@ -0,0 +1,5 @@
+package com.laioffer.l3_doOnNext.use1.bean;
+
+// 登录响应
+public class LoginResponse {
+}
---
+++ rx/src/main/java/com/laioffer/l3_doOnNext/use1/bean/RegisterResponse.java
@@ -0,0 +1,5 @@
+package com.laioffer.l3_doOnNext.use1.bean;
+
+// 注册响应
+public class RegisterResponse {
+}
