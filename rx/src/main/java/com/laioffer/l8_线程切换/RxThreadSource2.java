d3db6c5d2be326d359d76968e54133df162c1450
春夏秋冬在中南 <2541093882@qq.com>
2024-06-06 22:37:35 +0800

    [RxJava]2024/06/06

---
+++ rx/src/main/java/com/laioffer/l8_线程切换/RxThreadSource2.java
@@ -0,0 +1,50 @@
+package com.laioffer.l8_线程切换;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+/**
+ * 类描述: 线程切换
+ * <p>
+ * created by 西伯利亚哈士奇 on 2024/6/4 23:08
+ */
+public class RxThreadSource2 {
+
+  public static void main(String[] args) {
+    Observable.create(new ObservableOnSubscribe<String>() {
+          @Override
+          public void subscribe(@NonNull ObservableEmitter<String> emitter) throws Throwable {
+            emitter.onNext("线程切换");
+          }
+        })
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(
+            new Observer<String>() {
+              @Override
+              public void onSubscribe(@NonNull Disposable d) {
+
+              }
+
+              @Override
+              public void onNext(@NonNull String s) {
+
+              }
+
+              @Override
+              public void onError(@NonNull Throwable e) {
+
+              }
+
+              @Override
+              public void onComplete() {
+
+              }
+            });
+  }
+}
